5e661611a6b4f52ae89b66d42e80f770260a7817
nikran <nikran@gmail.com>
2021-11-03 16:55:16 +0100

    Create scripts.groovy

---
+++ scripts.groovy
@@ -0,0 +1,13 @@
+def test1(){
+    //add code for this method
+}
+def test2(){
+    //add code for this method
+}
+return thisdef test1(){
+    //add code for this method
+}
+def test2(){
+    //add code for this method
+}
+return this
